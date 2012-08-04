6cab4a366148c43f7807292d800aecbf5e702146
Tim Alkemade <timalkemade@gmail.com>
2012-08-04 15:57:38 +0200

    some changes

--- drive/nbproject/Makefile-Debug.mk
+++ drive/nbproject/Makefile-Debug.mk
@@ -45,6 +45,12 @@ OBJECTFILES= \
 	${OBJECTDIR}/WrappingArray.o \
 	${OBJECTDIR}/WrappingArrayIterator.o
 
+# Test Directory
+TESTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tests
+
+# Test Files
+TESTFILES= \
+	${TESTDIR}/TestFiles/f1
 
 # C Compiler Flags
 CFLAGS=
@@ -122,6 +128,158 @@ ${OBJECTDIR}/WrappingArrayIterator.o: WrappingArrayIterator.cpp
 
 # Subprojects
 .build-subprojects:
+
+# Build Test Targets
+.build-tests-conf: .build-conf ${TESTFILES}
+${TESTDIR}/TestFiles/f1: ${TESTDIR}/tests/newsimpletest.o ${OBJECTFILES:%.o=%_nomain.o}
+	${MKDIR} -p ${TESTDIR}/TestFiles
+	${LINK.cc}   -o ${TESTDIR}/TestFiles/f1 $^ ${LDLIBSOPTIONS} 
+
+
+${TESTDIR}/tests/newsimpletest.o: tests/newsimpletest.cpp 
+	${MKDIR} -p ${TESTDIR}/tests
+	${RM} $@.d
+	$(COMPILE.cc) -g -I. -I. -I. -MMD -MP -MF $@.d -o ${TESTDIR}/tests/newsimpletest.o tests/newsimpletest.cpp
+
+
+${OBJECTDIR}/DrivingController_nomain.o: ${OBJECTDIR}/DrivingController.o DrivingController.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/DrivingController.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/DrivingController_nomain.o DrivingController.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/DrivingController.o ${OBJECTDIR}/DrivingController_nomain.o;\
+	fi
+
+${OBJECTDIR}/RightWheel_nomain.o: ${OBJECTDIR}/RightWheel.o RightWheel.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/RightWheel.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/RightWheel_nomain.o RightWheel.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/RightWheel.o ${OBJECTDIR}/RightWheel_nomain.o;\
+	fi
+
+${OBJECTDIR}/drive_nomain.o: ${OBJECTDIR}/drive.o drive.pde 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/drive.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/drive_nomain.o drive.pde;\
+	else  \
+	    ${CP} ${OBJECTDIR}/drive.o ${OBJECTDIR}/drive_nomain.o;\
+	fi
+
+${OBJECTDIR}/LeftWheel_nomain.o: ${OBJECTDIR}/LeftWheel.o LeftWheel.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/LeftWheel.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/LeftWheel_nomain.o LeftWheel.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/LeftWheel.o ${OBJECTDIR}/LeftWheel_nomain.o;\
+	fi
+
+${OBJECTDIR}/fix_fft_nomain.o: ${OBJECTDIR}/fix_fft.o fix_fft.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/fix_fft.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/fix_fft_nomain.o fix_fft.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/fix_fft.o ${OBJECTDIR}/fix_fft_nomain.o;\
+	fi
+
+${OBJECTDIR}/Sensor_nomain.o: ${OBJECTDIR}/Sensor.o Sensor.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/Sensor.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/Sensor_nomain.o Sensor.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/Sensor.o ${OBJECTDIR}/Sensor_nomain.o;\
+	fi
+
+${OBJECTDIR}/DirectionSensor_nomain.o: ${OBJECTDIR}/DirectionSensor.o DirectionSensor.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/DirectionSensor.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/DirectionSensor_nomain.o DirectionSensor.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/DirectionSensor.o ${OBJECTDIR}/DirectionSensor_nomain.o;\
+	fi
+
+${OBJECTDIR}/Timer_nomain.o: ${OBJECTDIR}/Timer.o Timer.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/Timer.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/Timer_nomain.o Timer.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/Timer.o ${OBJECTDIR}/Timer_nomain.o;\
+	fi
+
+${OBJECTDIR}/WrappingArray_nomain.o: ${OBJECTDIR}/WrappingArray.o WrappingArray.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/WrappingArray.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/WrappingArray_nomain.o WrappingArray.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/WrappingArray.o ${OBJECTDIR}/WrappingArray_nomain.o;\
+	fi
+
+${OBJECTDIR}/WrappingArrayIterator_nomain.o: ${OBJECTDIR}/WrappingArrayIterator.o WrappingArrayIterator.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/WrappingArrayIterator.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} $@.d;\
+	    $(COMPILE.cc) -g -Dmain=__nomain -MMD -MP -MF $@.d -o ${OBJECTDIR}/WrappingArrayIterator_nomain.o WrappingArrayIterator.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/WrappingArrayIterator.o ${OBJECTDIR}/WrappingArrayIterator_nomain.o;\
+	fi
+
+# Run Test Targets
+.test-conf:
+	@if [ "${TEST}" = "" ]; \
+	then  \
+	    ${TESTDIR}/TestFiles/f1 || true; \
+	else  \
+	    ./${TEST} || true; \
+	fi
 
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
